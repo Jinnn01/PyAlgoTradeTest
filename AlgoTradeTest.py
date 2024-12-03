b58fc9b87ee021d1908d68e3d1e3a933042bacfd
Jinnn01 <60898623+Jinnn01@users.noreply.github.com>
2024-12-03 16:32:41 +1100

    print close prices as a strategy

--- AlgoTradeTest.py
+++ AlgoTradeTest.py
@@ -2,6 +2,7 @@ from pyalgotrade import strategy
 from pyalgotrade.barfeed import csvfeed
 from pyalgotrade.bar import Frequency
 
+# print close price
 class MyStrategy(strategy.BacktestingStrategy):
   def __init__(self,feed,instrument):
     super(MyStrategy,self).__init__(feed)
@@ -12,9 +13,16 @@ class MyStrategy(strategy.BacktestingStrategy):
     self.info(bar.getClose())
 
 
-# using basic feed
-feed2 = csvfeed.GenericBarFeed(frequency=Frequency.DAY)
-feed2.addBarsFromCSV('MSFT','MSFT_cleaned.csv')
+# Frequency.DAY, HOUR, MINUTE
+def runStrategy(name,csvfile,frequency):
+  feed = csvfeed.GenericBarFeed(frequency=frequency)
+  feed.addBarsFromCSV(name,csvfile)
+  myStrategy = MyStrategy(feed,name)
+  myStrategy.run()
 
-myStrategy = MyStrategy(feed2,'MSFT')
-myStrategy.run()
+print('-----AAPL-----')
+runStrategy('AAPL','AAPL1d5m_cleaned.csv',Frequency.MINUTE)
+print('-----MSFT-----')
+runStrategy('MSFT','MSFT3mo1d_cleaned.csv',Frequency.DAY)
+print('-----ORCL-----')
+runStrategy('ORCL','ORCL1mo1h_cleaned.csv',Frequency.HOUR)
--- yf_getcsv.py
+++ yf_getcsv.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 def downloadCSV(name,period,interval):
   stock_df = yf.download(name,period=period,interval=interval)
-  print(stock_df.columns)
 
   stock_df.reset_index(inplace=True)
   if 'Datetime' in stock_df.columns:
@@ -12,17 +11,17 @@ def downloadCSV(name,period,interval):
     stock_df.rename(columns={"Date": "Date Time"}, inplace=True)
 
   stock_df['Date Time'] = pd.to_datetime(stock_df["Date Time"]).dt.strftime("%Y-%m-%d %H:%M:%S")
-  stock_df.to_csv(name+'.csv',index=False)
+  stock_df.to_csv(name+period+interval+'.csv',index=False)
   
-  df = pd.read_csv(name+'.csv')
+  df = pd.read_csv(name+period+interval+'.csv')
   # drop empty value
   df_dropped= df.dropna()
-  df_dropped.to_csv(name+'_cleaned.csv',index=False)
+  df_dropped.to_csv(name+period+interval+'_cleaned.csv',index=False)
 
 
 downloadCSV('MSFT','3mo','1d')
-
 downloadCSV('ORCL','1mo','1h')
+downloadCSV('AAPL','1d','5m')
 
 
 
